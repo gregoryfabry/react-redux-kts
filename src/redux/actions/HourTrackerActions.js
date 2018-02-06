9f0ca80d2d2489c7932b30014099e814de003101
Adam Nolte <ajnolte12@gmail.com>
2018-02-05 18:36:16 -0600

    Change Wording

--- src/redux/actions/HourTrackerActions.js
+++ src/redux/actions/HourTrackerActions.js
@@ -3,7 +3,7 @@ export const ADD_ENTRY = 'ADD_ENTRY';
 export const FETCH_ENTRIES = 'FETCH_ENTRIES';
 export const FETCH_ENTRIES_SUCCESS = 'FETCH_ENTRIES_SUCCESS';
 
-export const fetchPermissions = (entry) => {
+export const addEntry = (entry) => {
   return { type: ADD_ENTRY, payload: {/* fill in*/}};
 };
 
