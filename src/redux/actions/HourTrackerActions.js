15b8eaa3440bce9f9fb7627e55437ab308d0bd5d
Gregory Fabry <gfabry@westmonroepartners.com>
2018-02-06 18:11:27 -0600

    update redux

--- src/redux/actions/HourTrackerActions.js
+++ src/redux/actions/HourTrackerActions.js
@@ -1,16 +1,13 @@
 export const ADD_ENTRY = 'ADD_ENTRY';
+export const UPDATE_ENTRY = 'UPDATE_ENTRY';
 
-export const FETCH_ENTRIES = 'FETCH_ENTRIES';
-export const FETCH_ENTRIES_SUCCESS = 'FETCH_ENTRIES_SUCCESS';
-
-export const addEntry = (entry) => {
-  return { type: ADD_ENTRY, payload: {/* fill in*/}};
+export const addEntry = () => {
+  return { type: ADD_ENTRY };
 };
 
-export const fetchEntries = () => {
-  return { type: FETCH_ENTRIES };
-}
-
-export const fetchEntriesSuccess = (entries) => {
-  return { type: FETCH_ENTRIES_SUCCESS, payload: entries };
-}
+export const updateEntry = (id, title, hours) => {
+  return {
+    type: UPDATE_ENTRY,
+    payload: { id, title, hours },
+  };
+};
